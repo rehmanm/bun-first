4ecf49859765df1469bd30c97d3fa1cdd74834ab
rehmanm <muhammad.abdulrehman@gmail.com>
2023-10-15 22:06:15 -0400

    basic routes added

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import figlet from "figlet";
+
+const server = Bun.serve({
+  port: 3000,
+  fetch(req) {
+    const url = new URL(req.url);
+    if (url.pathname === "/") {
+      const body = figlet.textSync("Hello World");
+      return new Response(body);
+    }
+    if (url.pathname === "/about") {
+      const body = figlet.textSync("About");
+      return new Response(body);
+    }
+
+    return new Response("404");
+  },
+});
+
+console.log(`Listening on PORT http://localhost:${server.port}`);
